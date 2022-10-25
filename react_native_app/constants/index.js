a3ac33f4e2c3eafd404b8f3cea40331c1cf32a9c
DevJanith <janithgamage1.ed@gmail.com>
2022-10-25 16:06:02 +0530

    fix: update

--- react_native_app/constants/index.js
+++
@@ -1,5 +0,0 @@
-import assets from "./assets";
-import { COLORS, SHADOWS, SIZES, FONTS } from "./theme";
-import { NFTData } from "./dummy";
-
-export { assets, COLORS, SHADOWS, SIZES, FONTS, NFTData };
