211d4265f0ead29a3c71595644425278e7bd3c2c
DevJanith <janithgamage1.ed@gmail.com>
2022-10-25 17:56:14 +0530

    feat: nft description page

--- template/react_native_app/components/index.js
+++ template/react_native_app/components/index.js
@@ -1,13 +1,19 @@
+import { CircleButton, RectButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
 import FocusedStatusBar from "./FocusedStatusBar";
 import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
-import { CircleButton, RectButton } from "./Button";
+import { SubInfo } from "./SubInfo";
 
 export {
     FocusedStatusBar,
     HomeHeader,
     NFTCard,
     CircleButton,
-    RectButton
+    RectButton,
+    SubInfo,
+    DetailsBid,
+    DetailsDesc
 };
 
