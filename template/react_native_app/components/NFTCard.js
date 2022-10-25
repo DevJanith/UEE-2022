3f3da013f25677a3b898c0e2167392813505b464
DevJanith <janithgamage1.ed@gmail.com>
2022-10-25 16:07:49 +0530

    fix: update desc: react native template and reference

---
+++ template/react_native_app/components/NFTCard.js
@@ -0,0 +1,61 @@
+import { useNavigation } from '@react-navigation/native'
+import React from 'react'
+import { Image, View } from 'react-native'
+
+import { assets, COLORS, SHADOWS, SIZES } from '../constants'
+import { CircleButton, RectButton } from './Button'
+import { SubInfo, EthPrice, NFTTitle } from './SubInfo'
+
+const NFTCard = ({ data }) => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={{
+      backgroundColor: COLORS.white,
+      borderRadius: SIZES.font,
+      marginBottom: SIZES.extraLarge,
+      margin: SIZES.base,
+      ...SHADOWS.dark
+    }}>
+      <View style={{ width: "100%", height: 250 }}>
+        <Image
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font
+          }}
+        />
+        <CircleButton imgUrl={assets.heart} right={10} top={10} />
+      </View>
+      <SubInfo />
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center"
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default NFTCard
---
+++ template/react_native_app/components/index.js
@@ -0,0 +1,13 @@
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+
+export {
+    FocusedStatusBar,
+    HomeHeader,
+    NFTCard,
+    CircleButton,
+    RectButton
+};
+
